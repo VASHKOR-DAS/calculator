868fe1382b6722982512f27e765228a7aea4a2d6
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-07-04 22:15:01 +0600

    edit

--- app.js
+++ app.js
@@ -7,49 +7,36 @@ let output = document.getElementById("output-value");
 // }
 
 
+const backspace = document.getElementById("backspace");
+backspace.addEventListener('click', function () {
 
+    output.innerText = output.innerText.substring(0, output.innerText.length - 1);
+})
+
+//class name
 let number = document.getElementsByClassName("number");
+
 for (let i = 0; i < number.length; i++) {
     number[i].addEventListener('click', function () {
         output.innerText += number[i].innerText;
     })
+    
 }
-
-
 
 let operator = document.getElementsByClassName("operator");
 
 for (let i = 0; i < operator.length; i++) {
-    operator[i].addEventListener('click', function () {
-
-        if (this.id == "clear") { //operator er vitorer jei id ta click korbe = this
-            output.innerText = "";
-            history.innerText = "";
-        }
-
-        else if (this.id == "backspace") {
-            output.innerText = output.innerText.substring(0, output.innerText.length - 1);
-        }
-
-        
-        else if (this.id = "=") {
-            output.innerText = parseFloat(output.innerText) + parseFloat(output.innerText);
-        }
-        
-        // else if  (this.id != "=") {
-        //     history.innerText = output.innerText + operator[i].innerText;
-        //     output.innerText = "";
-        // }
-        
-        
-
-
-
-
-
-
-
-
-
+    const element = operator[i];
+    element.addEventListener('click', function () {
+        output.innerText += operator[i].innerText;
     })
 }
+
+
+
+
+
+
+
+//output.innerText = parseFloat(output.innerText) + parseFloat(number[i].innerText);
+//         
